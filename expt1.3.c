28c72dedd1667f268ec53849e040135c1701cc5d
Gokulsai00 <challagokul00@gmail.com>
2025-10-06 11:19:12 +0530

    add

---
+++ expt1.3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char ch[100];
+    printf("enter your name ");
+    scanf("%s",&ch);
+    printf("name:%c\n",ch);
+    int age;
+    printf("enter your AGE ");
+    scanf("%d",&age);
+    printf("age is %d",age);
+return 0;
+}
